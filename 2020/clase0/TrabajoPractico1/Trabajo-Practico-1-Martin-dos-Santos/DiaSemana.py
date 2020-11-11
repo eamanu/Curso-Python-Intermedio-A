f21cf23436b822e6d4f12366656a71cc6e97af3f
Martin dos Santos <mds.designss@gmail.com>
2020-11-11 22:05:02 +0000

    Update DiaSemana.py

--- 2020/clase0/TrabajoPractico1/Trabajo-Practico-1-Martin-dos-Santos/DiaSemana.py
+++ 2020/clase0/TrabajoPractico1/Trabajo-Practico-1-Martin-dos-Santos/DiaSemana.py
@@ -1,19 +1,19 @@
-import datetime
-def diaSemana(dato="DD/MM/YYYY HH:MM"):
-    
-    dato=dato.split(' ')
-    fecha=[]
-    dias=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
-    hora=[]
-    
-    if(dato[0].find('/')>0):
-        if(dato[0].find('-')>0):
-            print("Escriba la fecha tipo DD/MM/YYYY, no se permiten guiones -")        
-        else:
-            fecha=dato[0].split('/')
-            if(dato[1].find(':')):
-                hora=dato[1].split(':')
-                print(dias[datetime.datetime(int(fecha[2]),int(fecha[1]),int(fecha[0]),int(hora[0]),int(hora[1])).weekday()])
-            else:
-                print("El formato de hora ingresado es incorrecto. Debe ser HH:MM")
+from datetime import datetime,date
+fecha=""
+
+while(fecha!="#"):
+    fecha = input("Introducir fecha (dd/mm/aaaa) o utilice el # para salir:")
+    if(fecha!="#"):
+        try:
+            formato = "%d/%m/%Y"
+            fecha2 = datetime.strptime(fecha,formato)
+        except ValueError:
+            formato = "%d-%m-%Y"
+            fecha2 = datetime.strptime(fecha,formato)
+        dia=date.isoweekday(fecha2)
+        dias=["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"]
+        print(dias[dia])
+    else: 
+        print("El programa finalizó")
+
     
